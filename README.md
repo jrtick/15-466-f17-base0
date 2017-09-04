b950a1dc2ce9872b1237f4badb340ae2afa5f910
Jordan Tick <jrtick@andrew.cmu.edu>
2017-09-03 22:38:34 -0400

    Updated readme and got basic game running. Need to fix bugs and make professional but basically done

--- README.md
+++ README.md
@@ -1,84 +1,23 @@
-# Base0
-
-Base0 is the starter code for the game0 in the 15-466-f17 course. It was developed by Jim McCann, and is hereby released into the public domain.
-
-Note that Base0 is attempting to illustrate a very straightfoward "get it done" philosophy of game code design -- I've purposefully removed some of the conveniences one typically uses to sweep things under the rug.
-
-This is because game0 *does not need* such conveniences. It is simple because game0 is simple.
+##Forked from Base0, which was written by Jim McCann for 15-466-f17 course.
+Modified by Jordan Tick and released into public domain.
 
 ## Requirements
-
  - modern C++ compiler
  - glm
  - libSDL2
 
 On Linux or OSX these requirements should be available from your package manager without too much hassle.
+I used linux, which first required
+	apt-get install libsdl2-dev libglm-dev
+Then I modified the makefile to required the std=c++11 and now it runs!
 
-## Building (Local Libraries)
+Sheperd Dog Game:
+*Initial state: five sheep in a pentagon around center, dog in bottom-right
+*Sheep get random NSEW direction
 
-If you are building on a system where you are unable to install libraries, you may wish to use one of the pre-built library repositories:
+*if sheep collide, move opposite their collision face
+*if hunter collides with sheep, changes its direction
+*collision with OOB and sheep results in game over
+*sheep speed gradually increases
 
-### Linux
-
-Clone [kit-libs-linux](https://github.com/ixchow/kit-libs-linux) into the `kit-libs-linux` subdirectory:
-```
-  git clone https://github.com/ixchow/kit-libs-linux
-```
-Now you can build using the '.local' Makefile:
-```
-  make -f Makefile.local
-```
-
-### OSX
-
-Clone [kit-libs-osx](https://github.com/ixchow/kit-libs-osx) into the `kit-libs-osx` subdirectory:
-```
-  git clone https://github.com/ixchow/kit-libs-osx
-```
-Now you can build using the '.local' Makefile:
-```
-  make -f Makefile.local
-```
-
-### Windows
-
-Clone [kit-libs-win](https://github.com/ixchow/kit-libs-win) into the `kit-libs-win` subdirectory:
-```
-  git clone https://github.com/ixchow/kit-libs-win
-```
-Now you can:
-```
-  nmake -f Makefile.win
-```
-or:
-```
-  cl.exe /EHsc /W3 /WX /MD /Ikit-libs-win\out\include /Ikit-libs-win\out\include\SDL2 main.cpp Draw.cpp gl_shims.cpp /link /SUBSYSTEM:CONSOLE /LIBPATH:kit-libs-win\out\lib SDL2main.lib SDL2.lib OpenGL32.lib
-  copy kit-libs-win\out\dist\SDL2.dll .
-```
-
-
-## Building (System Libraries)
-
-If you are building on a system where you can install libraries easily, then you may wish to build against versions of the required libraries installed by your favorite package manager.
-
-### Linux
-```
-  g++ -g -Wall -Werror -o main main.cpp Draw.cpp `sdl2-config --cflags --libs` -lGL
-```
-or:
-```
-  make
-```
-
-### OSX
-```
-  clang++ -g -Wall -Werror -o main main.cpp Draw.cpp `sdl2-config --cflags --libs`
-```
-or:
-```
-  make
-```
-
-### Windows
-
-Every scheme I am aware of for "globally installing" libraries on Windows seems like a hack.
+*sheperd dog moves by mouse? Maybe keyboard instead
--- main.cpp
+++ main.cpp
@@ -6,6 +6,41 @@
 
 #include <chrono>
 #include <iostream>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+class Sheep {
+public:
+	static float speed; //all sheep have same speed
+	static float radius; //all sheep are the same size
+	static glm::vec3 color;
+	glm::vec2 pos,vel; //different pos & vel
+	Sheep(){}
+	Sheep(glm::vec2 pos, glm::vec2 vel){
+		this->pos = pos;
+		this->vel = vel;
+	}
+	bool collision(Sheep other){
+		//tl stands for top-left corner
+		glm::vec2 tl1 = this->pos - glm::vec2(radius,radius),
+		          tl2 = other.pos - glm::vec2(radius,radius);
+		return tl1.x < tl2.x + 2*radius && tl1.x + 2*radius > tl2.x &&
+		       tl1.y < tl2.y + 2*radius && tl1.y + 2*radius > tl2.y;
+	}
+	bool collision(glm::vec2 tl2,glm::vec2 bl2){
+		//tl stands for top-left corner, bl stands for bottom-left corner
+		glm::vec2 tl1 = this->pos - glm::vec2(radius,radius);
+		return tl1.x < bl2.x && tl1.x + 2*radius > tl2.x &&
+		       tl1.y < bl2.y && tl1.y + 2*radius > tl2.y;
+	}
+};
+//starting sheep params
+float Sheep::speed = 0.1;
+float Sheep::radius = 0.1;
+glm::vec3 Sheep::color = glm::vec3(0.5,0.5,0.5);
+#define SHEEP_COUNT 5
+
 
 int main(int argc, char **argv) {
 	//Configuration:
@@ -75,10 +110,41 @@ int main(int argc, char **argv) {
 	SDL_ShowCursor(SDL_DISABLE);
 
 	//------------  game state ------------
+	srand(time(NULL)); //random seed
 
+	//SHEEP
+	Sheep sheeps[SHEEP_COUNT];
+	for(int i=0;i<SHEEP_COUNT;i++){
+		int randnum = rand() % 4;
+		float angle = 2*3.14159265/SHEEP_COUNT*i;
+		glm::vec2 pos = glm::vec2(0.2*sin(angle),0.2*cos(angle));
+		switch(randnum){
+		case 0:
+			sheeps[i] = Sheep(pos,glm::vec2(Sheep::speed,0));
+			break;
+		case 1:
+			sheeps[i] = Sheep(pos,glm::vec2(-Sheep::speed,0));
+			break;
+		case 2:
+			sheeps[i] = Sheep(pos,glm::vec2(0,Sheep::speed));
+			break;
+		default:
+			sheeps[i] = Sheep(pos,glm::vec2(0,-Sheep::speed));
+			break;
+		}
+	}
+	float total_time = 0;
+
+	//dog
+	glm::vec2 dog = glm::vec2(1,-1);
 	glm::vec2 mouse = glm::vec2(0.0f, 0.0f);
-	glm::vec2 ball = glm::vec2(0.0f, 0.0f);
-	glm::vec2 ball_velocity = glm::vec2(0.5f, 0.5f);
+
+	//Out of Bounds
+	glm::vec2 boundaries[4];
+	boundaries[0] = glm::vec2(-0.8,0.8);
+	boundaries[1] = glm::vec2(0.8,0.8);
+	boundaries[2] = glm::vec2(0.8,-0.8);
+	boundaries[3] = glm::vec2(-0.8,-0.8);
 
 	//------------  game loop ------------
 
@@ -91,10 +157,10 @@ int main(int argc, char **argv) {
 			if (evt.type == SDL_MOUSEMOTION) {
 				mouse.x = (evt.motion.x + 0.5f) / float(config.size.x) * 2.0f - 1.0f;
 				mouse.y = (evt.motion.y + 0.5f) / float(config.size.y) *-2.0f + 1.0f;
-			} else if (evt.type == SDL_MOUSEBUTTONDOWN) {
+			}/* else if (evt.type == SDL_MOUSEBUTTONDOWN) {
 				ball = mouse;
 				ball_velocity = glm::vec2(0.5f, 0.5f);
-			} else if (evt.type == SDL_KEYDOWN && evt.key.keysym.sym == SDLK_ESCAPE) {
+			}*/ else if (evt.type == SDL_KEYDOWN && evt.key.keysym.sym == SDLK_ESCAPE) {
 				should_quit = true;
 			} else if (evt.type == SDL_QUIT) {
 				should_quit = true;
@@ -108,22 +174,47 @@ int main(int argc, char **argv) {
 		previous_time = current_time;
 
 		{ //update game state:
-			ball += elapsed * ball_velocity;
-			if (ball.x < -1.0f) ball_velocity.x = std::abs(ball_velocity.x);
-			if (ball.x >  1.0f) ball_velocity.x =-std::abs(ball_velocity.x);
-			if (ball.y < -1.0f) ball_velocity.y = std::abs(ball_velocity.y);
-			if (ball.y >  1.0f) ball_velocity.y =-std::abs(ball_velocity.y);
+			dog = mouse;
+			glm::vec2 pad = glm::vec2(0.1,0.1);
+			for(int i=0;i<SHEEP_COUNT;i++){
+				sheeps[i].pos += sheeps[i].vel * elapsed;
+				for(int j=0;j<i;j++){
+					if(sheeps[i].collision(sheeps[j]))
+						sheeps[i].vel *= -1;
+				}
+				if(sheeps[i].collision(dog-glm::vec2(Sheep::radius,Sheep::radius),dog+glm::vec2(Sheep::radius,Sheep::radius))) sheeps[i].vel *= -1;
+				
+				if(sheeps[i].collision(boundaries[0]-pad,boundaries[1]+pad) ||
+				   sheeps[i].collision(boundaries[1]-pad,boundaries[2]+pad) ||
+				   sheeps[i].collision(boundaries[2]-pad,boundaries[3]+pad) ||
+				   sheeps[i].collision(boundaries[3]-pad,boundaries[0]+pad))
+					should_quit = true;
+
+			}
+			
+			total_time += elapsed;
+			Sheep::speed += elapsed*0.1f;
 		}
 
 		//draw output:
-		glClearColor(0.0, 0.0, 0.0, 0.0);
+		glClearColor(0.0, 1.0, 0.0, 1.0);
 		glClear(GL_COLOR_BUFFER_BIT);
 
 
 		{ //draw game state:
 			Draw draw;
-			draw.add_rectangle(mouse + glm::vec2(-0.1f,-0.1f), mouse + glm::vec2(0.1f, 0.1f), glm::u8vec4(0xff, 0xff, 0x00, 0xff));
-			draw.add_rectangle(ball + glm::vec2(-0.05f,-0.05f), ball + glm::vec2(0.05f, 0.05f), glm::u8vec4(0xff, 0x00, 0xff, 0xff));
+			//draw out of bounds
+			glm::vec2 pad = glm::vec2(0.05,0.05);
+			glm::u8vec4 col = glm::u8vec4(0.5,0,0.5,1);
+			draw.add_rectangle(boundaries[0]-pad,boundaries[1]+pad,col);
+			draw.add_rectangle(boundaries[1]-pad,boundaries[2]+pad,col);
+			draw.add_rectangle(boundaries[2]-pad,boundaries[3]+pad,col);
+			draw.add_rectangle(boundaries[3]-pad,boundaries[0]+pad,col);
+
+			//draw sheep and dog
+			for(int i=0;i<SHEEP_COUNT;i++)
+				draw.add_rectangle(sheeps[i].pos-glm::vec2(Sheep::radius,Sheep::radius),sheeps[i].pos+glm::vec2(Sheep::radius,Sheep::radius),glm::u8vec4(0xff,0xff,0xff,0xff));
+			draw.add_rectangle(dog-glm::vec2(Sheep::radius,Sheep::radius),dog+glm::vec2(Sheep::radius,Sheep::radius),glm::u8vec4(0xff,0x00,0x00,0xff));
 			draw.draw();
 		}
 
